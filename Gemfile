d80af614224a1e525d97e42ac7bd7fb94f911d3a
ayaya <ayaya@ayaya.tw>
2011-09-26 12:18:34 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,79 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0'
+gem 'rake'
+gem 'mysql2'
+
+group :assets do
+  gem 'sass-rails'
+  gem 'coffee-rails'
+  gem 'uglifier'
+  gem 'compass', '0.12.alpha.0'
+end
+
+gem 'jquery-rails'
+gem 'rails-i18n'
+
+#gem 'omniauth', '~> 0.2.6'
+#gem 'cancan'
+#gem 'acts_as_list'
+
+# View helpers
+#gem 'css3buttons', git: 'git://github.com/thetron/css3buttons_rails_helpers.git'
+#gem 'slim-rails'
+#gem 'cells'
+
+# Form helpers
+#gem 'formtastic', '~> 2.0.0.rc3'
+#gem 'simple_form'
+#gem 'client_side_validations'
+
+# Query DSL
+#gem 'squeel'
+
+# Paginate
+#gem 'will_paginate'
+#gem 'kaminari'
+
+# Model versioning
+#gem 'paper_trail'
+
+# Deploy
+#gem 'thin'
+#gem 'capistrano'
+
+# Monitors
+#gem 'newrelic_rpm'
+#gem 'exception_notification'
+
+# Crontab
+#gem 'whenever', require: false
+
+# File upload
+#gem 'paperclip'
+
+# Memcached
+#gem 'dalli'
+
+# Redis
+#gem 'hiredis'
+#gem 'redis', require: ['redis', 'redis/connection/hiredis']
+#gem 'redis-objects', require: 'redis/objects'
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'guard'
+  gem 'rb-fsevent'
+  gem 'growl'
+  gem 'rspec-rails'
+  gem 'capybara'
+  gem 'shoulda-matchers', '~> 1.0.0.beta3'
+  gem 'valid_attribute'
+  gem 'factory_girl_rails'
+  gem 'guard-rspec'
+  gem 'spork', '~> 0.9.0.rc'
+  gem 'guard-spork'
+  gem 'annotate'
+  gem 'guard-annotate'
+  gem 'simplecov', require: false
+end
---
+++ rails31.rb
@@ -0,0 +1,103 @@
+remove_file 'README'
+remove_file 'public/index.html'
+remove_file 'app/assets/images/rails.png'
+
+def read_from_file(filename)
+  root = File.dirname(rails_template)
+  File.read(File.join(root, filename))
+end
+
+def uncomment(file, line)
+  gsub_file file, /#\s*#{line}/, line
+end
+
+# Gemfile
+remove_file 'Gemfile'
+file 'Gemfile', read_from_file('Gemfile')
+
+# RVM
+file '.rvmrc', 'rvm 1.9.2'
+append_file '.gitignore', '.rvmrc'
+
+# Using gems
+uncomment 'Gemfile', "gem 'slim-rails'" if use_slim = yes?('Use Slim? (yes/no)')
+uncomment 'Gemfile', "gem 'cancan'" if use_cancan = yes?('Use Cancan? (yes/no)')
+
+# Bundler
+run 'bundle install'
+
+# Rspec, Spork, and Guard
+generate 'rspec:install'
+append_file '.rspec', '--drb'
+
+append_file 'config/database.yml', <<-SQLITE3
+
+test: # in-memory test
+  adapter: sqlite3
+  encoding: utf8
+  database: ':memory:'
+SQLITE3
+run 'cp config/database.yml config/database.yml.example'
+
+run 'spork --bootstrap'
+remove_file 'spec/spec_helper.rb'
+file 'spec/spec_helper.rb', read_from_file('spec_helper.rb')
+gsub_file 'spec/spec_helper.rb', /APP_NAME/, app_name.camelize
+
+run 'guard init'
+run 'guard init spork'
+run 'guard init rspec'
+
+file 'spec/factories.rb', <<-FACTORIES
+FactoryGirl.define do
+  # Add your factories here, for example:
+  #
+  #sequence(:username) { |n| "username-\#{n}" }
+  #sequence(:password) { SecureRandom.hex(30) }
+  #
+  #factory :user do
+  #  username { Factory.next(:username) }
+  #  password { Factory.next(:password) }
+  #
+  #  factory :admin do
+  #    admin true
+  #  end
+  #end
+end
+FACTORIES
+
+# Annotate
+run 'guard init annotate'
+
+# Slim
+if use_slim
+  remove_file 'app/views/layouts/application.html.erb'
+  file 'app/views/layouts/application.html.slim', read_from_file('application.html.slim')
+  gsub_file 'app/views/layouts/application.html.slim', /APP_NAME/, app_name.camelize
+end
+
+# Cancan
+if use_cancan
+  generate 'cancan:ability'
+  file 'spec/models/ability_spec.rb', read_from_file('ability_spec.rb')
+end
+
+# TODO: setup these gems
+# * formtastic (add styles)
+# * kaminari (config, i18n, views)
+# * exception_notification
+# * whenever
+# * paperclip (spec helper)
+# * dally (session store and cache store in production)
+
+# Git
+git :init
+
+append_file '.gitignore', <<-IGNORE_FILES
+config/database.yml
+public/system/
+coverage/
+IGNORE_FILES
+
+git :add => '.'
+git :commit => '-m "initial commit"'
